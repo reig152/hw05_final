550acac920c3ddbb67f31f10192f646b28ad8681
Сергей Стребков <sergei-strebkov@mail.ru>
2023-02-01 10:11:42 +0300

    Review

--- yatube/posts/tests/test_urls.py
+++ yatube/posts/tests/test_urls.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from http import HTTPStatus
 from django.test import TestCase, Client
+from django.urls import reverse
 from posts.models import Post, Group
 
 
